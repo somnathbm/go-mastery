f4d7216bafc3e0426bb9eb409b8306ea79ae7a0d
somnathbm <somnathbm0@gmail.com>
2026-07-22 22:23:23 +0530

    day09: generic worker pool updated

--- day09/monitor/monitor.go
+++ day09/monitor/monitor.go
@@ -3,7 +3,6 @@ package monitor
 import (
 	"context"
 	"day09/health"
-	"day09/metrics"
 	"day09/workerpool"
 	"time"
 )
@@ -14,30 +13,9 @@ const WorkerPoolSize = 100
 const ChannelCapacity = 1000
 const RetryCount = 3
 
-var myMetrics metrics.Metrics
+// var myMetrics metrics.Metrics
 
-// Consumer code
-func checkWorker(ctx context.Context, resourceCh <-chan health.HealthChecker, resultsCh chan<- health.HealthStatus) {
-	for {
-		start := time.Now()
-		select {
-		case resource, ok := <-resourceCh:
-			if !ok {
-				return
-			}
-
-			status := retryWith(resource)
-			end := time.Since(start)
-			status.ResponseTime = end
-			// myMetrics.Increment(status.Severity)
-
-			resultsCh <- status
-		case <-ctx.Done():
-			return
-		}
-	}
-}
-
+// Retry wrapper for a service
 func retryWith(resource health.HealthChecker) health.HealthStatus {
 	var hStatus health.HealthStatus
 
@@ -60,49 +38,13 @@ func retryWith(resource health.HealthChecker) health.HealthStatus {
 
 // Driver code
 func CheckResources(ctx context.Context, resources []health.HealthChecker) <-chan health.HealthStatus {
-	// 1. Setup
-	jobsCh := make(chan health.HealthChecker, ChannelCapacity)
-	// resultsCh := make(chan health.HealthStatus, ChannelCapacity)
-
-	// var wg sync.WaitGroup
-
-	// // 2. Worker pool
-	// for i := 1; i <= WorkerPoolSize; i++ {
-	// 	wg.Go(func() {
-	// 		// call worker code
-	// 		checkWorker(ctx, jobsCh, resultsCh)
-	// 	})
-	// }
-
-	// // 3. Co-ordinator block for synchronization
-	// go func() {
-	// 	wg.Wait()
-	// 	close(resultsCh)
-	// }()
-
-	resultsCh := workerpool.Run(ctx, jobsCh, processResource)
-
-	// 4. Producer code
-	go func() {
-		defer close(jobsCh)
-
-		for _, resource := range resources {
-			select {
-			case <-ctx.Done():
-				return
-			case jobsCh <- resource:
-			}
-		}
-	}()
-
-	return resultsCh
+	return workerpool.Run(ctx, resources, processResource)
 }
 
 // job function
-func processResource(resource health.HealthChecker) health.HealthStatus {
-	status, err := resource.CheckHealth()
-	if err != nil {
-		// deal with it
-	}
+func processResource(resource health.HealthChecker, startTime time.Time) health.HealthStatus {
+	status := retryWith(resource)
+	end := time.Since(startTime)
+	status.ResponseTime = end
 	return status
 }
